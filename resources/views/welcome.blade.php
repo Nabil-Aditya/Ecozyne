912500ca5f96db5832f5132b5f5234dc6fecdfc9
nabil <nabiladitya2203@gmail.com>
2025-03-02 16:55:14 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel coba</title>
+        <title>Laravel coba kali</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
